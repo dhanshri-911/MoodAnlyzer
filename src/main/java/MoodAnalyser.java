aa6a46fe32eef09336e649bbca9900277bbc10ed
Dhanshri911 <dszingade911@gmail.com>
2022-04-06 16:50:08 +0530

    UC1 : Added Mood Analyse

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,39 @@
+public class MoodAnalyser {
+        private String message;
+        public MoodAnalyser() {
+            this.message = null;
+        }
+
+        public MoodAnalyser(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public String analyseMood() {
+            if (message.toLowerCase().contains("happy") || message.toLowerCase().contains("any")) {
+                return "HAPPY";
+            } else if (message.toLowerCase().contains("sad")) {
+                return "SAD";
+            } else {
+                return null;
+            }
+        }
+
+        public static void main(String args[]) {
+            System.out.println("Mood Analyser");
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            moodAnalyser.setMessage("I am Happy");
+            System.out.println(moodAnalyser.analyseMood());
+            moodAnalyser.setMessage("I am Sad");
+            System.out.println(moodAnalyser.analyseMood());
+        }
+    }
+
+
