a9372529ca460e0478148f2a86abb4acd05f4f9e
Dhanshri911 <dszingade911@gmail.com>
2022-04-23 18:23:25 +0530

    UC3:Added Invalid Mood

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,39 +1,48 @@
 public class MoodAnalyser {
-        private String message;
-        public MoodAnalyser() {
-            this.message = null;
-        }
+    private String message;
 
-        public MoodAnalyser(String message) {
-            this.message = message;
-        }
+    public MoodAnalyser() {
+        this.message = null;
+    }
 
-        public String getMessage() {
-            return message;
-        }
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
 
-        public void setMessage(String message) {
-            this.message = message;
-        }
+    public String getMessage() {
+        return message;
+    }
 
-        public String analyseMood() {
-            if (message.toLowerCase().contains("happy") || message.toLowerCase().contains("any")) {
-                return "HAPPY";
-            } else if (message.toLowerCase().contains("sad")) {
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        try {
+            if (message.toLowerCase().contains("sad")) {
                 return "SAD";
-            } else {
-                return null;
+            }
+            else {
+                return "HAPPY";
             }
         }
-
-        public static void main(String args[]) {
-            System.out.println("Mood Analyser");
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            moodAnalyser.setMessage("I am Happy");
-            System.out.println(moodAnalyser.analyseMood());
-            moodAnalyser.setMessage("I am Sad");
-            System.out.println(moodAnalyser.analyseMood());
+        catch (NullPointerException e) {
+            return "HAPPY";
         }
     }
 
+    public static void main(String args[]) {
+        System.out.println("Mood Analyser");
 
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+        moodAnalyser.setMessage("I am Happy");
+        System.out.println(moodAnalyser.analyseMood());
+
+        moodAnalyser.setMessage("I am Sad");
+        System.out.println(moodAnalyser.analyseMood());
+
+        moodAnalyser.setMessage(null);
+        System.out.println(moodAnalyser.analyseMood());
+    }
+}
